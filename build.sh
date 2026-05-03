bbeefead6b5b21c494b74fad07b3cfc0e547e837
Krishna <krishnavarmav2345@gmail.com>
2026-05-03 11:51:25 +0530

    first commit

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Build started"
+echo "Number one is $NUMBERONE"
+echo "Number two is $NUMBERTWO"
+sum=$(($NUMBERONE + $NUMBERTWO))
+echo "the sum of 2 nums is $sum"
+echo "build is completed"
